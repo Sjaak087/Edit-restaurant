7b3104c35647308d9e4609986d48b4f6915d7068
Lars kersten <u0454074371@gmail.com>
2026-08-29 22:01:19 +0200

    Add small update entry to UPDATES array
    Added a new small update entry with details about note-taking and restaurant deletion features.

--- updates.js
+++ updates.js
@@ -15,6 +15,12 @@
 
 const UPDATES = [
   {
+    title: 'small update', 
+    date: '29-08-2026', 
+    time: '22:00', 
+    info: 'ik heb toegevoegd dat je nu notities kunt maken je mag er max 20 hebben je kunt ze afvinken en daarna worden ze automatisch verwijdert de notities worden gedeeld met het hele restaurant en iedereen kan er een maken of avinken ook heb ik toegevoegd dat ik nu kan instellen dat je restaurant automatisch verwijdert wordt er zit nog een kleine bug in maar die wordt zo gefixt tot morgen!'
+  },     
+  {
     title: 'Middel grote update', 
     date: '27-08-2026', 
     time: '21:30', 
